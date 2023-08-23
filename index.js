e78e1600968c21ffed214adf396e93e0be4a2e5c
Merc5Dark <31131794siya@gmail.com>
2023-08-23 11:45:09 +0200

    nodejs

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const fs = require("fs");
+const fs = require("fs"); //file system
 
-fs.writeFile("message.txt", "Hello from NODEJS!", (err) => {
+// fs.writeFile("message.txt", "NodeJS!", (err) => {
+//     if (err) throw err;
+//     console.log("File Saved Successfully");
+// })
+
+// Native Node Module
+
+fs.readFile("./message.txt", "utf8", (err,data) => {
     if (err) throw err;
-    console.log("The file is saved successfully");
+    console.log(data);
 })
